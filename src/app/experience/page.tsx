40b69e60cf7e0a18bcb8b1809a343f41af901726
athroscf <fialloschris1@gmail.com>
2024-10-13 22:15:49 -0600

    feat: created expriences and projects page.

---
+++ src/app/experience/page.tsx
@@ -0,0 +1,12 @@
+import Experiences from "../components/Experiences";
+
+export default function ExperienceProjects() {
+  return (
+    <div className="rounded-lg bg-white bg-opacity-90 p-8 shadow-lg dark:bg-gray-800 dark:bg-opacity-90">
+      <h1 className="mb-6 text-center text-3xl font-bold text-gray-800 dark:text-white">
+        Experience & Projects
+      </h1>
+      <Experiences />
+    </div>
+  );
+}
