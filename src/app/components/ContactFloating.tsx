2d1218c0ed0d2915083f68fb992d6ba6df62c933
Christopher Fiallos <fialloschris1@gmail.com>
2024-10-23 13:06:18 -0600

    feat: added links and created ContactLinks component.

--- src/app/components/ContactFloating.tsx
+++ src/app/components/ContactFloating.tsx
@@ -1,18 +1,10 @@
-import { FiGithub, FiLinkedin, FiTwitter } from "react-icons/fi";
+import ContactLinks from "./ContactLinks";
 
 const ContactFloating = () => {
   return (
     <div className="fixed left-0 top-1/2 z-40 -translate-y-1/2 transform rounded-r-md bg-white p-3 shadow-md">
       <div className="flex flex-col space-y-4">
-        <a href="#" className="transition-colors hover:text-purple-600">
-          <FiLinkedin size={24} />
-        </a>
-        <a href="#" className="transition-colors hover:text-purple-600">
-          <FiTwitter size={24} />
-        </a>
-        <a href="#" className="transition-colors hover:text-purple-600">
-          <FiGithub size={24} />
-        </a>
+        <ContactLinks />
       </div>
     </div>
   );
