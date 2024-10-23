ec73838ee1d7be0fde191d0aa338e3571bab2db1
Christopher Fiallos <fialloschris1@gmail.com>
2024-10-23 13:17:32 -0600

    style: bg color

--- src/app/components/BouncingArrow.tsx
+++ src/app/components/BouncingArrow.tsx
@@ -2,7 +2,7 @@ import { FaArrowDown } from "react-icons/fa6";
 
 const BouncingArrow = () => {
   return (
-    <div className="mt-8 p-2 animate-bounce rounded-full bg-slate-400 text-3xl text-gray-600">
+    <div className="mt-8 animate-bounce rounded-full bg-slate-400 p-2 text-3xl text-gray-600">
       <FaArrowDown />
     </div>
   );
