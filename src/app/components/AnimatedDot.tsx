224bdb05c469d97af6ef703147d3bc6cd60adb8a
Christopher Fiallos <fialloschris1@gmail.com>
2024-10-22 18:20:20 -0600

    lint: fix

--- src/app/components/AnimatedDot.tsx
+++ src/app/components/AnimatedDot.tsx
@@ -6,20 +6,20 @@ const AnimatedDot = () => {
   useEffect(() => {
     const animationFrame = requestAnimationFrame(animate);
     return () => cancelAnimationFrame(animationFrame);
-  }, [])
+  }, []);
 
   const animate = () => {
     setPosition((prevPosition) => {
       const newPosition = (prevPosition + 1) % 100;
       requestAnimationFrame(animate);
       return newPosition;
-    })
-  }
+    });
+  };
 
   return (
-    <div className="w-6 h-16 border-2 border-current rounded-full relative overflow-hidden mx-auto my-8">
+    <div className="relative mx-auto my-8 h-16 w-6 overflow-hidden rounded-full border-2 border-current">
       <div
-        className="w-2 h-2 bg-current rounded-full absolute left-1/2 transform -translate-x-1/2"
+        className="absolute left-1/2 h-2 w-2 -translate-x-1/2 transform rounded-full bg-current"
         style={{ top: `${position}%` }}
       />
     </div>
